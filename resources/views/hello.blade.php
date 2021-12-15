8214fcd6a0117652b679c1013a4458b4bc5d26e4
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-15 13:52:20 +0100

    fix navbar

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Hello world!</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Hello')
+
+@section('content')
+    <h1>Hello World!</h1>
+@endsection
